bc13461938c02e591e4a3e356e3bed3d13e41731
xXBlackMaskXx <marciniuk.adrian123@gmail.com>
2019-07-31 19:26:24 +0200

    add "kill riot" option

--- blackarrow/.scripts/app/dmenu-messengers.sh
+++ blackarrow/.scripts/app/dmenu-messengers.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-case "$(printf "  Telegram  \n  Riot  \n  Cancel  " | dmenu -h 33 -i -p '  Run:  ')" in
-	"  Telegram  ") telegram-desktop;;
-	"  Riot  ")     riot-desktop;;
-	*)              exit 0
+case "$(printf " Telegram \n Riot \n Kill Riot \n Cancel " | dmenu -h 33 -i -p '  Run:  ')" in
+	" Telegram ")  telegram-desktop;;
+	" Riot ")      riot-desktop;;
+	" Kill Riot ") killall electron;;
+	*)             exit 0
 esac
