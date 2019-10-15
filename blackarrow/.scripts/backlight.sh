26d1e3f447e0fa3f3eedf577ea719b23dd667c8a
xXBlackMaskXx <marciniuk.adrian123@gmail.com>
2019-10-15 14:27:18 +0200

    Small visual changes

--- blackarrow/.scripts/backlight.sh
+++ blackarrow/.scripts/backlight.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-light -S $(printf " 100 \n 75 \n 50 \n 25 " | dmenu -i -p 'What screen brightness should I set?')
+light -S $(printf " 100 \n  75 \n  50 \n  25 " | dmenu -i -p 'What screen brightness should I set?')
