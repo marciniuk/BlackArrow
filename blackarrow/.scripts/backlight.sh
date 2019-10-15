12a31b034a57c512463533e7aac50b88bc843362
xXBlackMaskXx <marciniuk.adrian123@gmail.com>
2019-10-15 14:22:38 +0200

    Greater minimalism

--- blackarrow/.scripts/backlight.sh
+++ blackarrow/.scripts/backlight.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
 
-choice=$(printf " 100 \n 75 \n 50 \n 25 " | dmenu -i -p 'What screen brightness should I set?')
-
-light -S $choice
+light -S $(printf " 100 \n 75 \n 50 \n 25 " | dmenu -i -p 'What screen brightness should I set?')
