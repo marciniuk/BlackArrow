5519e719a71173cb935bf6f323fa8226e0ff3332
xXBlackMaskXx <marciniuk.adrian123@gmail.com>
2019-10-25 03:21:46 +0200

    Finally version

--- blackarrow/.scripts/quick-cleaning.sh
+++ blackarrow/.scripts/quick-cleaning.sh
@@ -4,12 +4,17 @@ case "$(printf "~/.yay-log\n~/.BA" | dmenu -h 33 -i -p 'Are you cleaning:')" in
 	"~/.yay-log") find .yay-log/* -mtime +30 -print -delete &&
 	              find .yay-log/seen/* -mtime +30 -print -delete;;
 	"~/.BA")      #Configs
-	                  mkdir ~/.BA/configi/$(date +%G-%m)
-					  echo "Moving folders:"
-					  find ~/.BlackArrowGit/* -print
-					  mv ~/.BlackArrowGit/Kopia\ $(date +%G-%m)* ~/.BA/configi/$(date +%G-%m)
-					  echo "Delete old files in ~/.BA/configi"
-					  find ~/.BA/configi -mtime +90 -print -delete 
-					  echo "Delete empty directories in ~/.BA/configi"
-					  find ~/.BA/configi -type d -empty -print -delete
+	              	mkdir ~/.BA/configi/$(date +%G-%m)
+	              	mv ~/.BlackArrowGit/Kopia\ $(date +%G-%m)* ~/.BA/configi/$(date +%G-%m)
+	              	echo "	Delete old files in ~/.BA/configi"
+	                	find ~/.BA/configi -mtime +90 -print -delete
+	              	echo "	Delete empty directories in ~/.BA/configi"
+	              		find ~/.BA/configi -type d -empty -print -delete
+	              #Installers
+	              	mkdir ~/.BA/instalator/$(date +%G-%m)
+	              	mv ~/.BlackArrowOldInstallerGit/Kopia\ $(date +%G-%m)* ~/.BA/instalator/$(date +%G-%m)
+	              	echo "	Delete old files in ~/.BA/instalator"
+	                	find ~/.BA/instalator -mtime +90 -print -delete
+	              	echo "	Delete empty directories in ~/.BA/instalator"
+	                	find ~/.BA/instalator -type d -empty -print -delete
 esac
