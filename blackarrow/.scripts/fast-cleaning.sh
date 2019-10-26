84c6afdc1d5306c585930ad5d01e778f3b08c599
xXBlackMaskXx <marciniuk.adrian123@gmail.com>
2019-10-26 23:47:37 +0200

    add new option ("ALL")

--- blackarrow/.scripts/fast-cleaning.sh
+++ blackarrow/.scripts/fast-cleaning.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-case "$(printf " ~/.yay-log\n ~/.BA\n Cancel" | dmenu -h 33 -i -p 'Are you cleaning:')" in
+case "$(printf " ~/.yay-log\n ~/.BA\n All\n Cancel" | dmenu -h 33 -i -p 'Are you cleaning:')" in
 	" Cancel") exit 0;;
 
 	" ~/.yay-log")
@@ -59,6 +59,67 @@ case "$(printf " ~/.yay-log\n ~/.BA\n Cancel" | dmenu -h 33 -i -p 'Are 
 		echo "	Delete empty directories in ~/.BA/instalator:"
 		if find ~/.BA/instalator -type d -empty -print -delete;
 			then echo "";
+			else echo "No folders to delete"; fi;;
+
+	" All")
+		#yay
+		echo "	Delete old files in ~/.yay-log:"
+		find .yay-log/* -mtime +30 -print -delete
+			echo ""
+		echo "	Delete old files in ~/.yay-log/seen:"
+		find .yay-log/seen/* -mtime +30 -print -delete
+			echo ""
+
+    	echo "	~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		"
+
+		#configi
+		if mkdir ~/.BA/configi/"$(date +%G-%m)" &> /dev/null;
+			then echo "	Created folder "'~/.BA/configi/'"$(date +%G-%m)""
+				";
+			else echo "	The folder "'~/.BA/configi/'"$(date +%G-%m)"" already exists
+				"; fi
+
+		if mv ~/.BlackArrowGit/Kopia\ "$(date +%G-%m)"* ~/.BA/configi/"$(date +%G-%m)" &> /dev/null;
+			then echo "	Moved folders to "'~/.BA/configi/'"$(date +%G-%m)""
+				";
+			else echo "	The "'~/.BlackArrowGit'" folder is empty
+				"; fi
+
+		echo "	Delete old files in ~/.BA/configi:"
+		if find ~/.BA/configi -mtime +90 -print -delete;
+			then echo "";
+			else echo "No files to delete"; fi
+
+		echo "	Delete empty directories in ~/.BA/configi:"
+		if find ~/.BA/configi -type d -empty -print -delete;
+			then echo "";
 			else echo "No folders to delete"; fi
+
+    	echo "	~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		"
+
+		#Installers
+		if mkdir ~/.BA/instalator/"$(date +%G-%m)" &> /dev/null;
+			then echo "	Created folder "'~/.BA/instalator/'"$(date +%G-%m)""
+				";
+			else echo "	The folder "'~/.BA/instalator/'"$(date +%G-%m)"" already exists
+				"; fi
+
+		if mv ~/.BlackArrowOldInstallerGit/Kopia\ "$(date +%G-%m)"* ~/.BA/instalator/"$(date +%G-%m)" &> /dev/null;
+			then echo "	Moved folders to "'~/.BA/instalator/'"$(date +%G-%m)""
+				";
+			else echo "	The "'~/.BlackArrowOldInstallerGit'" folder is empty
+				"; fi
+
+		echo "	Delete old files in ~/.BA/instalator:"
+		if find ~/.BA/instalator -mtime +90 -print -delete;
+			then echo "";
+			else echo "No files to delete"; fi
+
+		echo "	Delete empty directories in ~/.BA/instalator:"
+		if find ~/.BA/instalator -type d -empty -print -delete;
+			then echo "";
+			else echo "No folders to delete"; fi;;
 
 esac
