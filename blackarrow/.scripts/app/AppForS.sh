6bb71c3ae313aa054b899f1b0896000fa9224419
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2020-03-22 15:48:18 +0100

    Małe zmiany w kolorach + naprawa dmenu + dodania clipmenu

--- blackarrow/.scripts/app/AppForS.sh
+++ blackarrow/.scripts/app/AppForS.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-case "$(printf "  Steam  \n  Spotify  \n  Cancel  " | dmenu -h 33 -i -p '  Run:  ')" in
-	"  Steam  ")   steam;;
-	"  Spotify  ") spotify;;
-	*)             exit 0
+case "$(printf " clipmenu\n clipdel\n Steam\n Spotify\n Cancel" | dmenu -h 33 -i -p '  Run:  ')" in
+	" clipmenu") clipmenu;;
+	" clipdel")  clipdel -d ".*";;
+	" Steam  ")  steam;;
+	" Spotify")  spotify;;
+	*)            exit 0
 esac
