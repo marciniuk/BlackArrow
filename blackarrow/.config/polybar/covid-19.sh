5049f4b6522862a3808281a5416ee06a07bc1220
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2020-03-25 02:01:52 +0100

    New COVID-19 script

---
+++ blackarrow/.config/polybar/covid-19.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+download() {
+	      stats="$(curl -s "https://corona.lmao.ninja/countries/Poland")"
+	 todaycases="$(echo "$stats" | jq '.todayCases')"
+	todaydeaths="$(echo "$stats" | jq '.todayDeaths')"
+
+	echo " $todaycases  $todaydeaths" > ~/.cache/covid-19
+}
+
+ping -w 1 corona.lmao.ninja >/dev/null 2>&1 && download
+
+cat ~/.cache/covid-19
---
+++ blackarrow/.scripts/covid-19.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+case "$(printf " Poland \n New Zealand \n China \n Italy \n World " | dmenu -h 33 -i -p '  System  ')" in
+	" Poland ")      place=" Poland "      location="https://corona.lmao.ninja/countries/Poland";;
+	" New Zealand ") place=" New Zealand " location="https://corona.lmao.ninja/countries/New Zealand";;
+	" China ")       place=" China "       location="https://corona.lmao.ninja/countries/China";;
+	" Italy ")       place=" Italy "       location="https://corona.lmao.ninja/countries/Italy";;
+
+	" World ")       stats="$(curl -s "https://corona.lmao.ninja/all")"
+		             active="$(echo "$stats" | jq '.cases')"
+					 deaths="$(echo "$stats" | jq '.deaths')"
+					 recovered="$(echo "$stats" | jq '.recovered')"
+					 lastupdate="$(echo "$stats" | jq '.updated')"
+					 time="$(echo "$lastupdate" | cut -b -10)"
+					 time2="$(date -d @"$time")"
+					 printf "  $active \n  $deaths \n  $recovered \n  $time2 " | dmenu -h 33 -i -p ' World '
+					 exit 0;;
+
+	*)               exit 0
+esac
+
+     stats="$(curl -s "$location")"
+    active="$(echo "$stats" | jq '.active')"
+todaycases="$(echo "$stats" | jq '.todayCases')"
+    deaths="$(echo "$stats" | jq '.deaths')"
+ recovered="$(echo "$stats" | jq '.recovered')"
+
+case "$(printf "  $active \n  $todaycases \n  $deaths \n  $recovered \n  More info " | dmenu -h 33 -i -p "$place")" in
+	"  More info ") stats="$(curl -s $location)"
+	                 cases="$(echo "$stats" | jq '.cases')"
+	                 active="$(echo "$stats" | jq '.active')"
+	                 todaycases="$(echo "$stats" | jq '.todayCases')"
+					 recovered="$(echo "$stats" | jq '.recovered')"
+					 critical="$(echo "$stats" | jq '.critical')"
+	                 deaths="$(echo "$stats" | jq '.deaths')"
+					 todaydeaths="$(echo "$stats" | jq '.todayDeaths')"
+					 casesperonemillion="$(echo "$stats" | jq '.casesPerOneMillion')"
+					 deathsperonemillion="$(echo "$stats" | jq '.deathsPerOneMillion')"
+
+					 printf " Cases: $cases \n Active: $active \n Today cases: $todaycases \n Recovered: $recovered \n Critical: $critical \n Deaths: $deaths \n Today deaths: $todaydeaths \n C/M: $casesperonemillion \n D/M: $deathsperonemillion " | dmenu -h 33 -i -p "$place";;
+	*)               exit 0
+esac
+
