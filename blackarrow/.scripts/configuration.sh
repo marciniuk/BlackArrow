d9a40ae081e4b5cc3787e931f3018477c310d906
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2020-09-06 14:44:03 +0200

    idk

--- blackarrow/.scripts/configuration.sh
+++ blackarrow/.scripts/configuration.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-case "$(printf "  CPU \n  FAN \n  BAT " | dmenu -h 33 -i -p 'What do you want to configure?')" in
+case "$(printf "  CPU \n  FAN \n  RGB \n  BAT " | dmenu -h 33 -i -p 'What do you want to configure?')" in
    "  CPU ") bash ~/.scripts/dmenu-cpu.sh;;
    "  FAN ") bash ~/.scripts/dmenu-fan.sh;;
+   "  RGB ") bash ~/.scripts/dmenu-gamma.sh;;
    "  BAT ") bash ~/.scripts/dmenu-bat.sh;;
    *)         exit 0
 esac
